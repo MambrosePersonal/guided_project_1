871a4ff99ca835d2175f2bab5bb210102b049750
Jacob Boyce <jboyce313@gmail.com>
2024-01-23 16:47:44 -0500

    added film title and director to film.html

--- film.js
+++ film.js
@@ -1,6 +1,8 @@
 const sp = new URLSearchParams(window.location.search);
 const id = sp.get("id");
 let film;
+let filmTitle = document.querySelector(".film-title");
+let director = document.querySelector(".director");
 
 async function getFilm() {
   let url = ` https://swapi2.azurewebsites.net/api/films/${id}`;
@@ -8,8 +10,15 @@ async function getFilm() {
   try {
     film = await fetch(url).then((res) => res.json());
     console.log(film);
+    renderFilm();
   } catch (ex) {
     console.error("error getting film", ex.message);
   }
 }
+
+function renderFilm() {
+  filmTitle.textContent = film.title;
+  director.textContent = `Directed by ${film.director}`;
+}
+
 getFilm();
