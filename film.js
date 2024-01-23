fa7143ff512e89deeb30faae407c3733f005cc7d
Jacob Boyce <jboyce313@gmail.com>
2024-01-23 16:56:45 -0500

    made fetch call to characters by film

--- film.js
+++ film.js
@@ -1,6 +1,7 @@
 const sp = new URLSearchParams(window.location.search);
 const id = sp.get("id");
 let film;
+let characters;
 let filmTitle = document.querySelector(".film-title");
 let director = document.querySelector(".director");
 
@@ -9,7 +10,9 @@ async function getFilm() {
 
   try {
     film = await fetch(url).then((res) => res.json());
+    characters = await fetch(`${url}/characters`).then((res) => res.json());
     console.log(film);
+    console.log(characters);
     renderFilm();
   } catch (ex) {
     console.error("error getting film", ex.message);
