3799b560acac26cfb4513bd35a39537997bc5064
Jacob Boyce <jboyce313@gmail.com>
2024-01-23 16:58:56 -0500

    made fetch call for planets by film

--- film.js
+++ film.js
@@ -2,6 +2,7 @@ const sp = new URLSearchParams(window.location.search);
 const id = sp.get("id");
 let film;
 let characters;
+let planets;
 let filmTitle = document.querySelector(".film-title");
 let director = document.querySelector(".director");
 
@@ -11,8 +12,10 @@ async function getFilm() {
   try {
     film = await fetch(url).then((res) => res.json());
     characters = await fetch(`${url}/characters`).then((res) => res.json());
+    planets = await fetch(`${url}/planets`).then((res) => res.json());
     console.log(film);
     console.log(characters);
+    console.log(planets);
     renderFilm();
   } catch (ex) {
     console.error("error getting film", ex.message);
