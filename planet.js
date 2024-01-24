c8edc99df0f53fd19ebf66b01860dcc80c083853
MambrosePersonal <mr.mikeambrose@gmail.com>
2024-01-24 10:59:17 -0500

    final planet changes

--- planet.js
+++ planet.js
@@ -1,7 +1,9 @@
 const sp = new URLSearchParams(window.location.search); 
 const id = sp.get('id');
 let character;
-let film;
+let films;
+let filmsDiv = document.querySelector('.Films')
+let characterDiv = document.querySelector('.Character');
 let planet;
 let planetName = document.querySelector('#name');
 let climate  = document.querySelector('#climate');
@@ -15,11 +17,13 @@ async function getPlanet() {
 
   try {
     planet = await fetch(url).then((res) => res.json());
-    film = await fetch(`${url}/films`).then((res) => res.json());
+    films = await fetch(`${url}/films`).then((res) => res.json());
     characters = await fetch(`${url}/characters`).then((res) => res.json());
     console.log(planet);
     console.log(id);
     renderPlanet(planet);
+    renderFilm();
+    renderCharacters();
   }catch (ex) {
     console.error(`Error get Planet`, ex.message);
   }
@@ -33,4 +37,27 @@ const renderPlanet = (planet) => {
   diameter.textContent = planet.diameter;
   orbital_period.textContent = planet.orbital_period;
 }
+
+function renderCharacters() {
+  for (let character of characters) {
+    console.log(character);
+    const characterEl = document.createElement("a");
+    characterEl.textContent = character.name;
+    characterEl.setAttribute("href", `/character.html?id=${character.id}`);
+    characterEl.setAttribute("class", "character");
+    characterDiv.appendChild(characterEl);
+  }
+}
+
+function renderFilm() {
+  for (let film of films) {
+    console.log(film);
+    const filmEl = document.createElement("a");
+    filmEl.textContent = film.title;
+    filmEl.setAttribute("href", `/film.html?id=${film.id}`);
+    filmEl.setAttribute("class", "film");
+    filmsDiv.appendChild(filmEl);
+  }
+}
+
 getPlanet();
